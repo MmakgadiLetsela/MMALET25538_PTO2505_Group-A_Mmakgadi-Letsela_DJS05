936bee0345e0268092c59af154aeb6206f04ac43
MmakgadiLetsela <mmakgadi.letsela@gmail.com>
2026-03-29 14:35:00 +0200

    add jsdoc comments for app component

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,14 @@ import ShowDetail from "./pages/ShowDetail";
 import { Route, Routes } from "react-router-dom";
 
 /**
- * 
+ * @function App
+ * Root component of the podcast explorer app.
+ * - Wraps the application in the podcast provider context for global state. 
+ * - Includes the Header component, displayed on all pages.
+ * - Defines client-side routes using React Router.
+ * - Renders home page and showdetail page depending on route chosen.
+ *  
+ * @returns {JSX.Element} The application component with routing and context.
  */
 export default function App() {
   return (
